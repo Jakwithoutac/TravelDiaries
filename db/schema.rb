c488d3373e4545f87ee5eee175fd7bff3cd56f28
Jak <jakwatkins@gmail.com>
2016-06-19 14:25:14 +0100

    Migrate to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160609085238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.text     "country"
     t.text     "area"
